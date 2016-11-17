73867d1fdc193202895f65f00cda85cb462cf7c4
tingxins <tingxins@sina.com>
2016-11-18 02:28:30 +0800

    update demo.

--- TXScrollLabelViewDemo/TXScrollLabelViewDemo/ViewController.m
+++ TXScrollLabelViewDemo/TXScrollLabelViewDemo/ViewController.m
@@ -35,15 +35,22 @@
     for (int options = 0; options < 4; ++ options) {
         NSString *scrollTitle = @"If you don't control the image server you're using, you may not be able to change the URL when its content is updated. This is the case for Facebook avatar URLs for instance. In such case, you may use the SDWebImageRefreshCached flag. This will slightly degrade the performance but will respect the HTTP caching control headers";
         //options 是 TXScrollLabelViewType 枚举， 此处为了方便举例
-        TXScrollLabelView *scrollLabelView = [TXScrollLabelView tx_setScrollTitle:scrollTitle scrollType:options scrollVelocity:2.0 options:UIViewAnimationOptionTransitionFlipFromTop];
+        TXScrollLabelView *scrollLabelView = [TXScrollLabelView tx_setScrollTitle:scrollTitle scrollType:options scrollVelocity:1.0 options:UIViewAnimationOptionCurveEaseInOut];
+        
         //布局
-        scrollLabelView.tx_scrollContentSize = CGRectMake(50, 100 * (options + 0.7), 250, 30);
+        scrollLabelView.frame = CGRectMake(50, 100 * (options + 0.7), 300, 44);
         scrollLabelView.tx_centerX = [UIScreen mainScreen].bounds.size.width * 0.5;
+        
         //偏好设置
+        scrollLabelView.scrollInset = UIEdgeInsetsMake(0, 10 , 0, 10);
+        scrollLabelView.scrollSpace = 10;
+        scrollLabelView.font = [UIFont systemFontOfSize:15];
+        scrollLabelView.textAlignment = NSTextAlignmentCenter;
         scrollLabelView.backgroundColor = [UIColor blackColor];
         scrollLabelView.layer.cornerRadius = 5;
         [self.view addSubview:scrollLabelView];
         
+        //开始滚动
         [scrollLabelView beginScrolling];
         self.scrollLabelView = scrollLabelView;
     }
