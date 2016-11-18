13a6c2d113ef6c5018f5380ffa44fcac9d1120fc
tingxins <tingxins@sina.com>
2016-11-19 00:22:37 +0800

    update demo.

--- TXScrollLabelViewDemo/TXScrollLabelViewDemo/AppDelegate.m
+++ TXScrollLabelViewDemo/TXScrollLabelViewDemo/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,10 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[ViewController alloc] init]];
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
--- TXScrollLabelViewDemo/TXScrollLabelViewDemo/ViewController.m
+++ TXScrollLabelViewDemo/TXScrollLabelViewDemo/ViewController.m
@@ -27,33 +27,36 @@
 }
 
 - (void)setViewInfos {
+    self.view.backgroundColor = [UIColor whiteColor];
     self.navigationController.navigationBar.translucent = NO;
+    self.title = @"TXScrollLabelView";
 }
 
 - (void)setSubviews {
     
     for (int options = 0; options < 4; ++ options) {
         NSString *scrollTitle = @"If you don't control the image server you're using, you may not be able to change the URL when its content is updated. This is the case for Facebook avatar URLs for instance. In such case, you may use the SDWebImageRefreshCached flag. This will slightly degrade the performance but will respect the HTTP caching control headers";
-        //options 是 TXScrollLabelViewType 枚举， 此处为了方便举例
-        TXScrollLabelView *scrollLabelView = [TXScrollLabelView tx_setScrollTitle:scrollTitle scrollType:options scrollVelocity:1.0 options:UIViewAnimationOptionCurveEaseInOut];
+        //options 是 TXScrollLabelViewType 枚举类型， 此处仅为了方便举例
+        TXScrollLabelView *scrollLabelView = [TXScrollLabelView tx_setScrollTitle:scrollTitle scrollType:options scrollVelocity:3 options:UIViewAnimationOptionTransitionFlipFromTop];
+        [self.view addSubview:scrollLabelView];
         
-        //布局
-        scrollLabelView.frame = CGRectMake(50, 100 * (options + 0.7), 300, 44);
-        scrollLabelView.tx_centerX = [UIScreen mainScreen].bounds.size.width * 0.5;
+        //布局(Required)
+        scrollLabelView.frame = CGRectMake(50, 100 * (options + 0.7), 300, 30);
         
-        //偏好设置
+        //偏好(Optional)
+        scrollLabelView.tx_centerX  = [UIScreen mainScreen].bounds.size.width * 0.5;
         scrollLabelView.scrollInset = UIEdgeInsetsMake(0, 10 , 0, 10);
         scrollLabelView.scrollSpace = 10;
         scrollLabelView.font = [UIFont systemFontOfSize:15];
         scrollLabelView.textAlignment = NSTextAlignmentCenter;
         scrollLabelView.backgroundColor = [UIColor blackColor];
         scrollLabelView.layer.cornerRadius = 5;
-        [self.view addSubview:scrollLabelView];
         
         //开始滚动
         [scrollLabelView beginScrolling];
         self.scrollLabelView = scrollLabelView;
     }
 }
+
 
 @end
